d01b53f7fb43ee0c8fa584de1bea6c2301a13954
andreablass <andreablass11@gmail.com>
2025-07-21 19:18:51 -0600

    WIP
      - Working on modal view

--- resources/js/views/Detox.vue
+++ resources/js/views/Detox.vue
@@ -2,8 +2,11 @@
 import { ref, onBeforeMount } from 'vue'
 import axios from 'axios'
 import { dataPagesStore } from '@/stores/dataPagesStore'
+import IngredientesModal from '@/components/IngredientesModal.vue'
 
 const store = dataPagesStore()
+const showModal = ref(false)
+const selectedItem = ref(null) // guarda el jugo seleccionado
 
 onBeforeMount(() => {
   // Solo hace fetch si no hay datos
@@ -11,34 +14,40 @@ onBeforeMount(() => {
     store.fetchAllPages()
   }
 })
+// Abrir modal con datos del jugo seleccionado
+function openModal(item) {
+  selectedItem.value = item
+  showModal.value = true
+}
 </script>
 
 <template>
   <div class="w-full flex justify-center px-4">
     <div class="w-full max-w-7xl py-10">
       <!-- Título principal -->
-      <h1 class="text-5xl font-extrabold text-yellow-800 mb-10">
-        Detox
-      </h1>
+      <h1 class="text-5xl font-extrabold text-yellow-800 mb-10">Detox</h1>
 
       <!-- Bloques de cada producto -->
-      <div
-        v-for="(item, index) in store.detox"
-        :key="item.title"
-        :class="index !== 0 ? 'border-t border-yellow-200 pt-6' : ''"
-        class="pb-6"
-      >
+      <div v-for="(item, index) in store.detox" :key="item.title" :class="index !== 0 ? 'border-t border-yellow-200 pt-6' : ''" class="pb-6">
         <!-- Nombre y Precio -->
         <div class="flex justify-between items-center mb-4">
           <div class="text-yellow-800 font-extrabold text-3xl">
-            {{ item.title }}
+            <button @click="openModal(item)" class="">
+              {{ item.title }}
+            </button>
           </div>
-          <div class="text-yellow-800 font-bold text-lg">
-            ${{ item.precio }}
-          </div>
+          <div class="text-yellow-800 font-bold text-lg">${{ item.precio }}</div>
         </div>
-
       </div>
     </div>
   </div>
+  <!-- Modal dinámico -->
+  <IngredientesModal
+    v-model="showModal"
+    v-if="selectedItem"
+    :title="selectedItem.title"
+    :descripcion="selectedItem.descripcion"
+    :sugerencia="selectedItem.sugerencia"
+    :imagen="selectedItem.imagen"
+  />
 </template>
